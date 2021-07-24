e53c6046cbb66b5aaf7d5e30d9913f680460b637
CharliB <barros.rcarlos@gmail.com>
2021-07-24 14:14:47 -0300

    se arregla problema para ver las imagenes

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
         require 'config/config.php';
         $Destino = new Destino();
         $destinos = $Destino->listarDestinos();
-	include 'includes/header.html';  
-	include 'includes/nav.php';  
+        include 'includes/header.php';
 ?>
 
 <main class="container mb-5">
-        <h1>Catálogo de productos</h1>
+        <h1>Destinos para viajar</h1>
 
         <section class="row">
 
@@ -15,12 +14,13 @@
         foreach ($destinos as $destino){
 ?>
             <div class="card col-lg-3 col-md-6 col-sm-12 ml-5 mb-5 mt-5 text-center">
-                <!-- <img src="productos/<?= $producto['prdImagen'] ?>" class="card-img-top"> -->
+                <img src="destinos/<?= $destino['imagen'] ?>" class="card-img-top mt-2"> 
                 <hr>
                     <h2><?= $destino['destNombre'] ?></h2>
-                        <?= $destino['regNombre'] ?> - <?= $producto['mkNombre'] ?> <br>
+                        <?= $destino['regNombre'] ?>  <br>
                         $<?= $destino['destPrecio']  ?>
-                <a href="detalles.php?destID=<?= $destino['destID'] ?>" class="btn btn-outline-info mb-1">Ver detalle</a>
+                        <!-- detalles.php?destID=<?= $destino['destID'] ?> -->
+                 <a href="" class="btn btn-outline-info mb-1">Ver detalle</a> 
             </div>
 <?php
 }
@@ -31,5 +31,5 @@
 </main>
 
 <?php
-include 'includes/footer.php';
+  include 'includes/footer.php';
 ?>
